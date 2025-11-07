5ad75ff6d109d3f3c70b8756b1ed9a71e7f7667f
Farzan Khan <farzankhan61r@gmail.com>
2025-11-07 23:56:53 +0530

    test out methods from scratch

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,6 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: "Click me to visit google"
+}
+
+function MyApp() {
+    return (
+        <div>
+        <h1>Custom React App</h1>
+        </div>
+    )
+}
 createRoot(document.getElementById('root')).render(
-    <App />
+    MyApp()
 )
