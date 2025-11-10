adf7e88e5d8fbc9fdd150d5c2f9d444ea25f1c3e
Farzan Khan <farzankhan61r@gmail.com>
2025-11-10 20:10:30 +0530

    add custom card using tailwind

---
+++ tailwindProps/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <img src="https://imgs.search.brave.com/iRjhiAuRJt-LANYbfN3CPn3XiGulpPGSazcul96O1vQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtY3NlLmNhbnZh/LmNvbS9ibG9iLzEy/MTg2NTUvZmVhdHVy/ZV9TdG9ja0ltYWdl/c19sZWFkXzAzMngu/anBn" alt="" />
+        <h1 className='text-3xl bg-green-500 p-3 rounded'>A card for photos</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus culpa obcaecati vero.</p>
+    </div>
+  )
+}
+
+export default Card     
