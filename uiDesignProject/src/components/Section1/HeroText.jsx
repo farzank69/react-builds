96afd66444af8a07208a5846ae1add83d7719fd3
Farzan Khan <farzankhan61r@gmail.com>
2025-11-11 04:15:54 +0530

    add multiple section

---
+++ uiDesignProject/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+        <h3 className='mb-7 text-7xl font-bold'>Prospective <br /> <span> customer  </span><br /> segmentation</h3>
+        <p className='text-xl font-medium text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, assumenda natus explicabo laborum quia beatae at dicta ullam recusandae est!</p>
+    </div>
+  )
+}
+
+export default HeroText
---
+++ uiDesignProject/src/components/Section1/Section1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Content from './Content'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full'>
+        <Navbar/>
+        <Content/>
+    </div>
+
+  )
+}
+
+export default Section1
