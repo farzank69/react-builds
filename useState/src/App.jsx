98625605cd2311f3f9b70fa2ccddc51a4f5f56c9
Farzan Khan <farzankhan61r@gmail.com>
2025-11-12 02:23:25 +0530

    batch update, event handling

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -2,24 +2,52 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
+  // destructuring
+  // const [user, setUser] = useState({name:'Farzan', age:22})
   const [num, setNum] = useState(10)
-  const increment = () => {
-    setNum(num+1)
+  const btnClicked = () => {
+    // const newUser = {...user} // created a copy of user but not the same refrence now the refrence is different -> destructing the object or array
+    // newUser.name = 'Kratos'
+    // newUser.age = 44
+    // setUser(newUser)
+    // -> Batch Update : we can fix this as well | The value won't increase by 3 but we can fix this up 
+    // setNum(num+1)
+    // setNum(num+1)
+    // setNum(num+1)
+    // Fixing the issue and its called batch update.
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
   }
-  const decrement = () => {
-    setNum(num-1)
-  }
-  const jump = () => {
-    setNum(num+5)
-  }
+
   return (
     <div>
+      {/* <h1>{user.name} {user.age}</h1> */}
       <h1>{num}</h1>
-      <button onClick={increment}>Increase</button>
-      <button onClick={decrement}>Decrease</button>
-      <button onClick={jump}>Jump by 5</button>
+      <button onClick={btnClicked}>click</button>
     </div>
   )
+
+
+
+  // const [num, setNum] = useState(10)
+  // const increment = () => {
+  //   setNum(num+1)
+  // }
+  // const decrement = () => {
+  //   setNum(num-1)
+  // }
+  // const jump = () => {
+  //   setNum(num+5)
+  // }
+  // return (
+  //   <div>
+  //     <h1>{num}</h1>
+  //     <button onClick={increment}>Increase</button>
+  //     <button onClick={decrement}>Decrease</button>
+  //     <button onClick={jump}>Jump by 5</button>
+  //   </div>
+  // )
 
 
 
