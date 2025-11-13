b7f43df2e52ab996e43d3ffcad4e667b21df9412
Farzan Khan <farzankhan61r@gmail.com>
2025-11-13 18:17:49 +0530

    use tailwind and usestate

---
+++ bg-changer/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <div className='h-screen w-full duration-200' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={()=>{setColor('red')}} className='bg-red-500 active:scale-50 outline-none px-4 py-1 rounded-full shadow-lg text-black'>Red</button>
+          <button onClick={()=>{setColor('blue')}} className=' bg-blue-500 active:scale-50 outline-none px-4 py-1 rounded-full shadow-lg text-black'>Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
