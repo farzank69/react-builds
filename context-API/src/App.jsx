d3bca3db30c877ab0ea2de5d6ec117f8fe626d20
Farzan Khan <farzankhan61r@gmail.com>
2025-11-19 23:59:14 +0530

    tailwind setup

--- context-API/src/App.jsx
+++ context-API/src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-white bg-black'>Test the context api</h1>
-      <p>Just a testing of tailwind</p>
+      
     </div>
   )
 }
